3b52766aa95b28f93e919887456a5797f6d00f0e
FabrizioCoder <fabrizioivan36@gmail.com>
2024-11-30 10:55:06 -0600

    Remove unused components from default layout for cleaner structure

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,15 +14,12 @@ body {
 <script lang="ts">
 import Navbar from '../components/Navbar.vue';
 import Sidebar from '../components/Sidebar.vue';
-import MainContent from '../components/MainContent.vue';
-import Facebook from '../components/Facebook.vue';
 import Footer from '../components/Footer.vue';
 
 export default {
   components: {
-    Facebook,
+    Navbar,
     Sidebar,
-    MainContent,
     Footer,
   },
 };
